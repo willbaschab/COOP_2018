d2779f380e80244935228644422c5375e9aa9060
willbaschab <will.b21@student.parishepiscopal.org>
2018-10-01 11:42:45 -0600

    commit 3.2 exercises

---
+++ Chapter02/practice01.py
@@ -0,0 +1,24 @@
+# practice01.py
+#
+#  Author: Will Baschab
+#  Course: Coding for OOP
+# Section: A2
+#    Date: 01 Oct 2018
+#     IDE: PyCharm
+#
+# Assignment Info
+#   Exercise:
+#     Source: Python Programming
+#    Chapter:
+#
+# Program Description
+#
+#
+#
+#
+# Algorithm (pseudocode)
+#
+#
+#
+#
+#
