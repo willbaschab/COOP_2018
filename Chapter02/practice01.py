bfa6ae48b297bbb3d3f441d66f1c54d05d0d7fff
willbaschab <will.b21@student.parishepiscopal.org>
2018-10-01 17:16:59 -0600

    practice ch.2 commit

--- Chapter02/practice01.py
+++ Chapter02/practice01.py
@@ -7,18 +7,38 @@
 #     IDE: PyCharm
 #
 # Assignment Info
-#   Exercise:
+#   Exercise: 11
 #     Source: Python Programming
-#    Chapter:
+#    Chapter: 02
 #
 # Program Description
-#
-#
-#
+#   This program converts jewels to Calories.
 #
 # Algorithm (pseudocode)
-#
-#
-#
-#
-#
+#  print introduction
+#  Loop until exit requested
+#   get jewels as input from user (with 0 to quit)
+#   Test for exit condition (break if true)
+#   calculate jewels to Calories (1 jewel = 0.239 calories)
+#       Calories = jewels * 0.239
+#   Print calories in complete sentence.
+
+
+def main():
+    print('\nThis program converts jewels to Calories.')
+
+    # Loop until exit requested
+    while True:
+        jewels = float(input("\nEnter amount of jewels to convert (0 to quit): "))
+
+        # Test for exit condition (break if true)
+        if jewels == 0:
+            break
+
+        Calories = jewels * 0.239
+
+        print()
+        print(jewels, "jewels is equal to", Calories, "Calories.")
+
+
+main()
--- Chapter02/practice02.py
+++ Chapter02/practice02.py
@@ -7,18 +7,37 @@
 #     IDE: PyCharm
 #
 # Assignment Info
-#   Exercise:
+#   Exercise: 12
 #     Source: Python Programming
-#    Chapter:
+#    Chapter: 02
 #
 # Program Description
-#
+#  This program is an interactive calculator that accepts mathematical expressions
 #
 #
 #
 # Algorithm (pseudocode)
+# Print introduction
+# Begin while loop for true
+#   get user input as a string and check for condition exit
+#   check for "exit" and quit loop if true
+#   expr is equal to evaluating expression
+#   print evaluated expression
 #
-#
-#
-#
-#
+
+
+def main():
+    print("\nThis program is an interactive calculator that accepts mathematical expressions.")
+
+    while True:
+        expression = str(input("\nEnter expression to calculate (type exit to exit): "))
+
+        if expression == "exit":
+            break
+
+        expr = eval(expression)
+
+        print()
+        print(expression, "=", expr)
+
+main()
